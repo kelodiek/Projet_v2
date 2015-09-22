8024598641317d5dacd4821c8a0af82232199a5d
Élodie <kerel1330243@etu.cegepjonquiere.ca>
2015-09-22 18:34:40 -0400

    test pour david

--- Sprint 1/Package 1/Code/P01_IHM_03_fixed(2)/P01_IHM_03_fixed/Projet/frmDetClassification.cs
+++ Sprint 1/Package 1/Code/P01_IHM_03_fixed(2)/P01_IHM_03_fixed/Projet/frmDetClassification.cs
@@ -12,13 +12,73 @@ namespace Projet
 {
     public partial class frmDetClassification : frmDetail
     {
+        Classification classification;
         ControleClassification cc;
         public frmDetClassification()
         {
             InitializeComponent();
             this.PositionBtn(144);
             btnActiverModif.Click += new EventHandler(btnActiverModif_Click);
+            this.btnEnregistrer.Click += new EventHandler(enregistrer);
+            this.btnSupprimer.Click += new EventHandler(btnSupprimer_Click);
+            this.btnCopier.Click += new EventHandler(btnCopier_Click);
             cc = new ControleClassification();
+        }
+
+        private void enregistrer(object sender, EventArgs e)
+        {
+            //Categorie enregistrement = new Categorie();
+            //bool resulVerif;
+            //DialogResult resultEnrg;
+
+            //enregistrement.codeCateg = txtCode.Text.Trim();
+            //enregistrement.descCateg = txtDesc.Text.Trim();
+            //enregistrement.comCateg = rtxtCommentaire.Text.Trim();
+
+            //resulVerif = ctrlCateg.verifier(enregistrement, categSelect);
+            
+
+  
+            //if (categSelect != null && ((string)Tag) != "Copie" )
+            //{
+                
+            //    if (resulVerif)
+            //    {
+            //        resultEnrg = MessageBox.Show("Voulez-vous vraiment enregister?", "Enregistrement", MessageBoxButtons.YesNo);
+            //        if (resultEnrg == DialogResult.Yes)
+            //        {
+            //            ctrlCateg.modifier(enregistrement);
+            //            this.Close();
+            //        }
+                    
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Aucune modification n'a été apportée.", "Erreur", MessageBoxButtons.OK);
+            //    }
+            //}
+            //else
+            //{
+            //    if (!ctrlCateg.testExiste(enregistrement.codeCateg) && txtCode.Text.Trim().Length != 0)
+            //    {
+            //        resultEnrg = MessageBox.Show("Voulez-vous vraiment enregister?", "Enregistrement", MessageBoxButtons.YesNo);
+            //        if (resultEnrg == DialogResult.Yes)
+            //        {
+            //            ctrlCateg.ajouter(enregistrement);
+            //            this.Close();
+            //        }
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Une categorie avec ce code existe deja ou le code est trop court.", "Erreur", MessageBoxButtons.OK);
+            //    }
+            //}
+            
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         public void modifierChamp(string cote, string nom, string desc)
@@ -30,6 +90,7 @@ namespace Projet
             txtNom.Text = nom;
             txtDescription.Text = desc;
             this.btnEnregistrer.Enabled = false;
+            classification = new Classification(cote, nom, desc);
         }
 
         public void modifierChamp()
@@ -52,5 +113,7 @@ namespace Projet
             txtNom.ReadOnly = false;
         }
 
+
+        public EventHandler btnCopier_Click { get; set; }
     }
 }
