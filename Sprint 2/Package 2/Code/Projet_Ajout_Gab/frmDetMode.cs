7cd8f9ce6eb4f6f7f93804f056a2e56a9a1768bb
lochagen <gab_sim16@hotmail.com>
2015-10-01 09:02:44 -0400

    Modif mod et genre

---
+++ Sprint 2/Package 2/Code/Projet_Ajout_Gab/frmDetMode.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet
+{
+    public partial class frmDetMode : frmDetail
+    {
+        public Mode modeSelect { get; set; }
+        private ctrlMode ctrlMo;
+        public bool annuler { get; set; }
+
+        public frmDetMode()
+        {
+            InitializeComponent();
+            ctrlMo = new ctrlMode();
+            this.PositionBtn(260);
+            this.txtNom.ReadOnly = false;
+            this.rtxtDesc.ReadOnly = false;
+            this.btnEnregistrer.Click += new EventHandler(btnEnregistrer_Click);
+            this.btnSupprimer.Click += new EventHandler(btnSupprimer_Click);
+            this.btnCopier.Click += new EventHandler(btnCopier_Click);
+            this.btnActiverModif.Click += new EventHandler(btnActiverModif_Click);
+            annuler = true;
+        }
+
+        public frmDetMode(Mode M)
+        {
+            InitializeComponent();
+            modeSelect = M;
+            ctrlMo = new ctrlMode();
+            this.PositionBtn(260);
+            this.btnEnregistrer.Click += new EventHandler(btnEnregistrer_Click);
+            this.btnCopier.Click += new EventHandler(btnCopier_Click);
+            annuler = true;
+        }
+
+        public void btnCopier_Click(object sender, EventArgs e)
+        {
+            Mode cop = new Mode();
+            frmDetMode frmCop;
+
+            cop.nomMode = txtNom.Text;
+            cop.descMode = rtxtDesc.Text;
+
+            frmCop = new frmDetMode(cop);
+            frmCop.Tag = "Copie";
+            frmCop.remplirChamp();
+            frmCop.btnCopier.Enabled = false;
+            frmCop.btnSupprimer.Enabled = false;
+            frmCop.btnActiverModif.Enabled = false;
+            frmCop.txtNom.ReadOnly = false;
+            frmCop.rtxtDesc.ReadOnly = false;
+            frmCop.ShowDialog();
+            if (frmCop.annuler == false)
+            {
+                this.Close();
+            }
+            frmCop.Closed += (s, args) => this.Close();
+        }
+
+        public void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            DialogResult resultEnrg = MessageBox.Show("Voulez-vous vraiment supprimer?", "Suppression", MessageBoxButtons.YesNo);
+            if (resultEnrg == DialogResult.Yes)
+            {
+                ctrlMo.supprimer(txtID.Text);
+                this.Close();
+            }
+        }
+
+        public void btnEnregistrer_Click(object sender, EventArgs e)
+        {
+            Mode enregistrement = new Mode();
+            bool resultVerif;
+            DialogResult resultEnr;
+
+            enregistrement.nomMode = txtNom.Text.Trim();
+            enregistrement.descMode = rtxtDesc.Text.Trim();
+
+            resultVerif = ctrlMo.verifier(enregistrement, modeSelect);
+
+            if (resultVerif)
+            {
+                if (modeSelect != null && ((string)Tag) != "Copie")
+                {
+                    resultEnr = MessageBox.Show("Voulez-vous vraiment enregistrer?", "Enregistrement", MessageBoxButtons.YesNo);
+                    if (resultEnr == DialogResult.Yes)
+                    {
+                        enregistrement.idMode = Int32.Parse(txtID.Text.Trim());
+                        ctrlMo.modifier(enregistrement);
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    if (ctrlMo.verifSemblable(enregistrement, modeSelect) == true)
+                    {
+                        resultEnr = MessageBox.Show("Voulez-vous vraiment enregistrer?", "Enregistrement", MessageBoxButtons.YesNo);
+                        if (resultEnr == DialogResult.Yes)
+                        {
+                            ctrlMo.ajouter(enregistrement);
+                            annuler = false;
+                            this.Close();
+                        } 
+                    }
+                }
+            }
+        }
+        public void modifierChamp(string code)
+        {
+            if (code == "a")
+            {
+                this.btnActiverModif.Visible = false;
+                modeSelect = null;
+            }
+            else
+            {
+                this.txtID.ReadOnly = true;
+                this.rtxtDesc.ReadOnly = true;
+                this.txtNom.ReadOnly = true;
+                remplirChamp();
+            }
+        }
+
+        private void remplirChamp()
+        {
+            txtID.Text = modeSelect.idMode.ToString();
+            txtNom.Text = modeSelect.nomMode;
+            rtxtDesc.Text = modeSelect.descMode;
+        }
+
+        private void btnActiverModif_Click(object sender, EventArgs e)
+        {
+            this.txtNom.ReadOnly = false;
+            this.rtxtDesc.ReadOnly = false;
+            ((Button)sender).Enabled = false;
+        }
+    }
+}
---
+++ Sprint 2/Package 2/Code/Projet_Ajout_Gab/frmGesGenre.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Projet
+{
+    partial class frmGesGenre
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GridGenre = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.GridGenre)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GridGenre
+            // 
+            this.GridGenre.AllowUserToAddRows = false;
+            this.GridGenre.AllowUserToResizeRows = false;
+            this.GridGenre.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridGenre.Location = new System.Drawing.Point(12, 71);
+            this.GridGenre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.GridGenre.MultiSelect = false;
+            this.GridGenre.Name = "GridGenre";
+            this.GridGenre.ReadOnly = true;
+            this.GridGenre.RowTemplate.Height = 24;
+            this.GridGenre.Size = new System.Drawing.Size(1059, 369);
+            this.GridGenre.TabIndex = 45;
+            this.GridGenre.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GridGenre_CellClick);
+            this.GridGenre.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GridGenre_CellDoubleClick);
+            // 
+            // frmGesGenre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 500);
+            this.Controls.Add(this.GridGenre);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmGesGenre";
+            this.Text = "Gestion des Genres";
+            this.Load += new System.EventHandler(this.frmGesGenre_Load);
+            this.Controls.SetChildIndex(this.GridGenre, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.GridGenre)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.DataGridView GridGenre;
+    }
+}
+
