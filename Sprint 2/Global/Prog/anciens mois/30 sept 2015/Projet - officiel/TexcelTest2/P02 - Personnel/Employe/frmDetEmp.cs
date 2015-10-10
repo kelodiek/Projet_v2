3e170852df52c35fdc8effe05912d86a53d5a442
Élodie <kerel1330243@etu.cegepjonquiere.ca>
2015-10-10 11:42:48 -0400

    ajout de ma version de jeu un peu plus avancé

---
+++ Sprint 2/Global/Prog/anciens mois/30 sept 2015/Projet - officiel/TexcelTest2/P02 - Personnel/Employe/frmDetEmp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Projet;
+
+namespace Projet
+{
+    public partial class frmDetEmp : frmDetail
+    {
+        public frmDetEmp()
+        {
+            InitializeComponent();
+            btnCopier.Visible = false;
+            this.btnAnnuler.Location = new Point(840, 658);
+
+        }
+    }
+}
