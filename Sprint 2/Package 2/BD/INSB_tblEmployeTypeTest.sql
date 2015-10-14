ca4ebb8a79f59532faf669b16e39f30d75e9534c
AlexPed1 <pedal1233143@etu.cegepjonquiere.ca>
2015-10-14 13:33:37 -0400

    INSC-INSB Script Insertion - + Modif DOC
    1:00 PM à 5:00 PM le mardi le 2015-10-13

---
+++ Sprint 2/Package 2/BD/INSB_tblEmployeTypeTest.sql
@@ -0,0 +1,28 @@
+USE dbProjetE2Prod;
+GO
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+INSERT INTO Personnel.tblEmployeTypeTest(IdEmp,IdTypeTest) values(,);
+GO
+USE master;
