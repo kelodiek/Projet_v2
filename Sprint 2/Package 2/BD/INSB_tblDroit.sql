9f4440403bd8a5456b1fe69cc20c09bf73f040c0
AlexPed1 <pedal1233143@etu.cegepjonquiere.ca>
2015-10-25 14:48:29 -0400

    AJOJUT DE DROIT

--- Sprint 2/Package 2/BD/INSB_tblDroit.sql
+++ Sprint 2/Package 2/BD/INSB_tblDroit.sql
@@ -6,5 +6,7 @@ INSERT INTO Personnel.tblDroit(CodeDroit,DescDroit) values('RP02','Droit lecture
 INSERT INTO Personnel.tblDroit(CodeDroit,DescDroit) values('WP02','Droit écriture sur tout le contenu employé');
 INSERT INTO Personnel.tblDroit(CodeDroit,DescDroit) values('RP03','Droit lecture sur tout le contenu test');
 INSERT INTO Personnel.tblDroit(CodeDroit,DescDroit) values('WP03','Droit écriture sur tout le contenu de test');
+INSERT INTO Personnel.tblDroit(CodeDroit,DescDroit) values('RConsoleAdmin','Droit lecture sur la console d"administration');
+INSERT INTO Personnel.tblDroit(CodeDroit,DescDroit) values('WConsoleAdmin','Droit écriture sur la console d"administration');
 GO
 USE master;
