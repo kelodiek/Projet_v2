2a42ec6a5d0624f6d052253c14374b9238af507f
Élodie <kerel1330243@etu.cegepjonquiere.ca>
2015-10-29 11:57:34 -0400

    BD -_-

---
+++ Sprint 3/Package 3/BD/INST_tblProjet_Test.sql
@@ -0,0 +1,10 @@
+USE dbProjetE2Test;
+GO
+INSERT INTO Test.tblProjet(CodeProjet, TitreProjet) VALUES ('TEST1','TestProjet1');
+INSERT INTO Test.tblProjet(CodeProjet, TitreProjet) VALUES ('TEST2','TestProjet2');
+INSERT INTO Test.tblProjet(CodeProjet, TitreProjet) VALUES ('TEST3','TestProjet3');
+INSERT INTO Test.tblProjet(CodeProjet, TitreProjet) VALUES ('TEST4','TestProjet4');
+INSERT INTO Test.tblProjet(CodeProjet, TitreProjet) VALUES ('TEST5','TestProjet5');
+INSERT INTO Test.tblProjet(CodeProjet, TitreProjet) VALUES ('TEST6','TestProjet6');
+GO
+USE master;
