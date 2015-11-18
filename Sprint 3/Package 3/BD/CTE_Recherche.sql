20c5e7df16cbf35c7d6e31f9360e6ff707b9daad
AlexPed1 <pedal1233143@etu.cegepjonquiere.ca>
2015-11-18 16:04:20 -0500

    Script
    CRE - Creation des tables Filtre
    CTE - Contrainte des tables Filtres
    FCT - TOUTE LES VIEWS des TABLES EN SELECT * POUR L'INSTANT

---
+++ Sprint 3/Package 3/BD/CTE_Recherche.sql
@@ -0,0 +1,27 @@
+use dbProjetE2Test;
+GO
+
+ALTER TABLE Test.tblFiltre1
+ADD CONSTRAINT PK_tblFiltre1_IdFiltre1	PRIMARY KEY(IdFiltre1)
+PRINT '1- Création de la contrainte PK_tblFiltre1_IdFiltre1 reussie'
+GO
+
+ALTER TABLE Test.tblFiltre2
+ADD CONSTRAINT PK_tblFiltre2_IdFiltre2	PRIMARY KEY(IdFiltre2)
+PRINT '1- Création de la contrainte PK_tblFiltre2_IdFiltre2 reussie'
+GO
+
+ALTER TABLE Test.tblFiltre3
+ADD CONSTRAINT PK_tblFiltre3_IdFiltre3	PRIMARY KEY(IdFiltre3)
+PRINT '1- Création de la contrainte PK_tblFiltre3_IdFiltre3 reussie'
+
+
+ALTER TABLE Test.tblFiltre2
+ADD CONSTRAINT FK_tblFiltre1_tblFiltre2_IdFiltre1	FOREIGN KEY(IdFiltre1) REFERENCES Test.tblFiltre1(IdFiltre1)
+PRINT '2- Création de la contrainte FK_tblFiltre1_tblFiltre2_IdFiltre1 reussie'
+GO
+
+ALTER TABLE Test.tblFiltre3
+ADD CONSTRAINT FK_tblFiltre2_tblFiltre3_IdFiltre2	FOREIGN KEY(IdFiltre2) REFERENCES Test.tblFiltre2(IdFiltre2)
+PRINT '2- Création de la contrainte FK_tblFiltre2_tblFiltre3_CodeProjet reussie'
+GO
