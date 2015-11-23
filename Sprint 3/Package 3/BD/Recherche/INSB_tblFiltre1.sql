38bfc6e50977829b35e38c8e4b09f7ab30ee8fa1
AlexPed1 <pedal1233143@etu.cegepjonquiere.ca>
2015-11-23 11:04:15 -0500

    Script pour Recherche

---
+++ Sprint 3/Package 3/BD/Recherche/INSB_tblFiltre1.sql
@@ -0,0 +1,13 @@
+USE dbProjetE2Test;
+GO
+INSERT INTO Test.tblFiltre1(NomFiltre1,DescFiltre1,NomVue) VALUES ('Employe','','VEmployeRecherche');
+INSERT INTO Test.tblFiltre1(NomFiltre1,DescFiltre1,NomVue) VALUES ('Equipe','','VEquipeRecherche');
+INSERT INTO Test.tblFiltre1(NomFiltre1,DescFiltre1,NomVue) VALUES ('Projet','','VProjetRecherche');
+INSERT INTO Test.tblFiltre1(NomFiltre1,DescFiltre1,NomVue) VALUES ('Version','','VVersionRecherche');
+INSERT INTO Test.tblFiltre1(NomFiltre1,DescFiltre1,NomVue) VALUES ('Jeu','','VJeurecherche');
+INSERT INTO Test.tblFiltre1(NomFiltre1,DescFiltre1,NomVue) VALUES ('Plateforme','','VPlateformeRecherche');
+INSERT INTO Test.tblFiltre1(NomFiltre1,DescFiltre1,NomVue) VALUES ('Type Test','','VTypeTestRecherche');
+
+GO
+USE master;
+
